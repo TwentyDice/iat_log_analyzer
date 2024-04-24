67bd81d5bfaee270280f7fb368df88a7df6fd5a2
TwentyDice <TwentyDice@users.noreply.github.com>
2024-04-24 19:44:43 +0200

    asdf asdf

--- gui_manager.py
+++ gui_manager.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Manage GUI interactions
 # Create and destroy a window per function, because I don't know how to use this properly, and am scared of memory leaks
 import tkinter as tk
@@ -25,31 +24,3 @@ def get_dir_with_info_msgbox(msg=None):
     root.destroy()
 
     return dir
-=======
-# Manage GUI interactions
-# Create and destroy a window per function, because I don't know how to use this properly, and am scared of memory leaks
-import tkinter as tk
-from tkinter import filedialog
-
-_TITLE = "20Dice IAT Log Analyzer"
-
-def show_error_msgbox(msg=None):
-    root = tk.Tk()
-    root.withdraw()
-    tk.messagebox.showerror(title=_TITLE, message=msg)
-    root.destroy()
-
-    return
-
-
-def get_dir_with_info_msgbox(msg=None):
-
-    root = tk.Tk()
-    root.withdraw()
-    tk.messagebox.showinfo(
-        title=_TITLE, message=msg, )
-    dir = filedialog.askdirectory()
-    root.destroy()
-
-    return dir
->>>>>>> 936311e68c79d0d76d19439edc02c6445ddef2dc
--- testing.py
+++ testing.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 from datetime import datetime as dt
 print(dt.now().strftime("%Y-%m-%d_%H-%M-%S"))
-=======
-from datetime import datetime as dt
-print(dt.now().strftime("%Y-%m-%d_%H-%M-%S"))
->>>>>>> 936311e68c79d0d76d19439edc02c6445ddef2dc
